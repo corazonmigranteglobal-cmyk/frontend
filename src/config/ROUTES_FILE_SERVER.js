41e2f80d226ed14edee4ee55db97a1932624743b
Pablo Arauz Caballero <128090351+PabloArauzCaballero@users.noreply.github.com>
2026-02-18 18:07:33 -0400

    fix

--- src/config/ROUTES_FILE_SERVER.js
+++ src/config/ROUTES_FILE_SERVER.js
@@ -5,6 +5,6 @@ export const ROUTES_FILE_SERVER = {
     URL_OCUPACIONES: "https://storage.googleapis.com/vistas_publicas_assets/landing_page/text_content/OCUPACIONES.json",
     URL_SINTOMAS: "https://storage.googleapis.com/vistas_publicas_assets/landing_page/text_content/SINTOMAS_PSICOLOGICOS_PSIQUIATRICOS.json",
     URL_OBJETIVOS: "https://storage.googleapis.com/vistas_publicas_assets/landing_page/text_content/OBJETIVOS_TERAPIA.json",
-    URL_AUTH: "https://storage.googleapis.com/vistas_publicas_assets/admin_portal/media/Banner%20portal%20administrativo",
+    URL_AUTH: "https://storage.googleapis.com/vistas_publicas_assets/landing_page/media/Dise_o_sin_t_tulo_7__c464ecd0-68bd-4a35-afed-c7bad56c7670.png",
     URL_LOGO: "https://storage.googleapis.com/vistas_publicas_assets/global_assets/media/LOGO%20CORAZON%20MIGRANTE.png"
 }
